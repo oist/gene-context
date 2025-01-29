a0ea94013557e726673ef22faa178456f1e043e7
AnzhelikaKoldaeva <anzhelika.koldaevaak@gmail.com>
2025-01-29 10:59:48 +0900

    minor changes

--- set_transformer/set_transformer_architecture.py
+++ set_transformer/set_transformer_architecture.py
@@ -10,11 +10,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class SetTransformer(nn.Module): #768 512
     def __init__(self, dim_input, num_outputs, dim_output,
-            num_inds=num_inds, dim_hidden=512, num_heads=4, ln=True):  #num_inds is the number of inducing points m dim_hidden=512
+            num_inds=num_inds, dim_hidden=32, num_heads=4, ln=True):  #num_inds is the number of inducing points m dim_hidden=512 ln=True
         super(SetTransformer, self).__init__()
         self.enc = nn.Sequential(
                 ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
-                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+              #  ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
                 nn.Dropout(0.3))
         self.dec = nn.Sequential(
                 PMA(dim_hidden, num_heads, num_outputs, ln=ln),
@@ -28,7 +28,7 @@ class SetTransformer(nn.Module): #768 512
 
 
 
-class SetTransformerWrapper(nn.Module):
+class SetTransformerWrapperOld(nn.Module):
     def __init__(self, model, output_class):
         super(SetTransformerWrapper, self).__init__()
         self.model = model
@@ -37,3 +37,16 @@ class SetTransformerWrapper(nn.Module):
     def forward(self, x):
         outputs = self.model(x)  # Get model outputs
         return outputs[:, self.output_class]  # Select the class output        
+
+
+class SetTransformerWrapper(nn.Module):
+    def __init__(self, model, output_class):
+        super(SetTransformerWrapper, self).__init__()
+        self.model = model
+        self.output_class = output_class
+
+    def forward(self, x):
+        logits = self.model(x)  # Get model outputs (logits)
+        probs = torch.nn.functional.softmax(logits, dim=-1)  # Convert logits to probabilities
+        return probs[:, self.output_class]  # Select the class output as probability
+        
