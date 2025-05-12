81441cac1aaece3a886e9b6d41f5830a18679f59
AnzhelikaKoldaeva <anzhelika.koldaevaak@gmail.com>
2025-05-12 18:28:30 +0900

    ogt stuff

--- phenotype_prediction/jupyter_notebooks/utils/utils.py
+++ phenotype_prediction/jupyter_notebooks/utils/utils.py
@@ -575,7 +575,7 @@ def train_xgboost_classification(X_train, y_train, X_test, y_test, num_classes=5
 
 
 
-def train_xgboost(X_train, y_train, X_test, y_test, weights = None):
+def train_xgboost(X_train, y_train, X_test, y_test, weights = None, model = None):
     # model = XGBRegressor(
     # n_jobs=-1,                # Use all CPU cores
     # tree_method="hist",   # Use "hist" for CPU, "gpu_hist" for GPU
@@ -583,7 +583,12 @@ def train_xgboost(X_train, y_train, X_test, y_test, weights = None):
     # n_estimators=500, 
     # )
 
-    model = XGBRegressor(reg_alpha=1.0,reg_lambda=1.0, max_depth=3,subsample=0.8, colsample_bytree=0.8, n_estimators=300,learning_rate=0.05)
+    if model is None:
+        model = XGBRegressor(reg_alpha=1.0,reg_lambda=1.0, max_depth=3,subsample=0.8, colsample_bytree=0.8, n_estimators=300,learning_rate=0.05)
+    else:
+        model = model    
+
+    
 
     # Define cross-validation (e.g., 5-fold)
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
